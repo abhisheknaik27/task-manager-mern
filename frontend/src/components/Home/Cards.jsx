f8b19b3daf8f00e75311e52bb7aad86552e8b4aa
abhisheknaik27 <abhisheknaik2704@gmail.com>
2024-11-16 01:49:16 +0530

    added add-icon on AllTasks Page

--- frontend/src/components/Home/Cards.jsx
+++ frontend/src/components/Home/Cards.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CiHeart } from "react-icons/ci";
 import { FaEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
@@ -9,21 +9,27 @@ const Cards = () => {
   const data = [
     {
         title: 'Go to gym',
-        desc: "from 7-10"
+        desc: "from 7-10",
+        status: "Incomplete"
     },
     {
         title: 'Do project',
-        desc: "from 10-12"
+        desc: "from 10-12",
+        status: "Complete"
     },
     {
         title: 'Study',
-        desc: "from 1-4"
+        desc: "from 1-4",
+        status: "Complete"
     },
     {
         title: 'Walking',
-        desc: "from 7-10"
+        desc: "from 7-10",
+        status: "Incomplete"
     },
   ];
+
+  
   return (
     <div className='grid grid-cols-3 gap-4 p-4'>
         {data && data.map((item, i) => (
@@ -33,7 +39,9 @@ const Cards = () => {
                     <p className='text-gray-300 my-2'>{item.desc}</p>
                 </div>
                 <div className='mt-4 w-full flex justify-between items-center'>
-                    <button className='bg-red-400 rounded p-2'>Incomplete</button>
+                    <button className={`${item.status == "Incomplete" ? 'bg-red-400' : 'bg-green-600'} rounded p-2`}>
+                        {item.status }
+                    </button>
                     <div className='text-white p-2 w-3/6 text-2xl font-semibold flex justify-around'>
                         <button><CiHeart /></button>
                         <button><FaEdit /></button>
--- frontend/src/pages/AllTasks.jsx
+++ frontend/src/pages/AllTasks.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Cards from '../components/Home/Cards'
+import { IoIosAddCircle } from "react-icons/io";
 
 const AllTasks = () => {
   return (
     <div>
+        <div className='w-full flex justify-end px-4 py-2'>
+            <button>
+              <IoIosAddCircle className='text-4xl text-gray-400 hover:text-gray-100 transition-all duration-300'/>
+            </button>
+        </div>
       <Cards />
     </div>
   )
