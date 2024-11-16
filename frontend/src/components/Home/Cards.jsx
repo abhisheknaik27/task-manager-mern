ae4cb00db19b3c1a9aed7ca45c548b0373c8d7e0
abhisheknaik27 <abhisheknaik2704@gmail.com>
2024-11-17 04:46:51 +0530

    complete incomplete toggle done

--- frontend/src/components/Home/Cards.jsx
+++ frontend/src/components/Home/Cards.jsx
@@ -3,19 +3,55 @@ import { CiHeart } from "react-icons/ci";
 import { FaEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 import { IoIosAddCircle } from "react-icons/io";
+import axios from 'axios';
 
-const Cards = ({ home, setInputDiv, data }) => {
+const Cards = ({ home, setInputDiv, data: initialData }) => {
+    const [status, setStatus] = useState(initialData || []);
 
+  const headers = { 
+    id: localStorage.getItem("id"), 
+    authorization: `Bearer ${localStorage.getItem("token")}`,
+  };
+  const handleCompleteTask = async(id) => {
+
+    setStatus(prevData =>
+        prevData.map(item =>
+          item._id === id ? { ...item, complete: !item.complete } : item
+        )
+      );
+
+    try{
+        const response = await axios.put(`http://localhost:3000/api/v2/updateCompleteTask/${id}`, 
+        {},
+        {headers}
+        );
+        console.log(response);
+    
+    } catch(err){
+        console.error("Failed to update task:", err);
+        alert("Something went wrong while updating the task.");
+        // Rollback in case of error
+        setStatus((prevData) =>
+            prevData.map((item) =>
+            item._id === id ? { ...item, complete: !item.complete } : item
+        )
+      );
+
+    }
+  }
   return (
     <div className='grid grid-cols-3 gap-4 p-4'>
-        {data && data.map((item, i) => (
-            <div className='flex flex-col justify-between bg-gray-800 rounded-sm p-4'>
+        {status && status.map((item, i) => (
+            <div key={item._id} className='flex flex-col justify-between bg-gray-800 rounded-sm p-4'>
                 <div>
                     <h3 className='text-xl font-semibold'>{item.title}</h3>
                     <p className='text-gray-300 my-2'>{item.description}</p>
                 </div>
                 <div className='mt-4 w-full flex justify-between items-center'>
-                    <button className={`${item.complete == false ? 'bg-red-400' : 'bg-green-600'} rounded p-2`}>
+                    <button 
+                    className={`${item.complete === false ? 'bg-red-400' : 'bg-green-600'} rounded p-2 w-3/6`}
+                    onClick={() => handleCompleteTask(item._id)}
+                    >
                         {item.complete === true ? "Completed" : "Incomplete"}
                     </button>
                     <div className='text-white p-2 w-3/6 text-2xl font-semibold flex justify-around'>
