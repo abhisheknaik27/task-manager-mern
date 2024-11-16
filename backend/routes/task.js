60d696c01017142109770c5793ea28bc065d7074
abhisheknaik27 <abhisheknaik2704@gmail.com>
2024-11-17 01:33:37 +0530

    all backend api completed

--- backend/routes/task.js
+++ backend/routes/task.js
@@ -21,6 +21,7 @@ router.post('/createTask', authenticateToken ,  async (req, res) => {
     }
 });
 
+//get all tasks
 router.get('/allTasks', authenticateToken, async(req, res) => {
     try{
         const { id } = req.headers;
@@ -30,6 +31,125 @@ router.get('/allTasks', authenticateToken, async(req, res) => {
     });
         
         res.status(200).json({ data: userData });
+    }catch(err){
+        console.log(err);
+        res.status(400).json({msg: "Internal Server Error"});
+    }
+});
+
+//delete task
+router.delete('/deleteTask/:id', authenticateToken, async(req, res) => {
+    try{
+        const { id } = req.params;
+        const userId = req.headers.id;
+        await TaskModel.findByIdAndDelete(id);
+        await UserModel.findByIdAndUpdate(userId, { $pull: {tasks: id}});
+        res.status(200).json({ msg: 'task deleted' });
+    }catch(err){
+        console.log(err);
+        res.status(400).json({msg: "Internal Server Error"});
+    }
+});
+
+//update Api
+router.put('/updateTask/:id', authenticateToken, async(req, res) => {
+    try{
+        const { id } = req.params;
+        const { title, description } = req.body;
+        await TaskModel.findByIdAndUpdate(id, { 
+            title: title,
+            description: description
+        });
+        res.status(200).json({ msg: 'task updated' });
+    }catch(err){
+        console.log(err);
+        res.status(400).json({msg: "Internal Server Error"});
+    }
+});
+
+//update imp tas
+router.put('/updateImpTask/:id', authenticateToken, async(req, res) => {
+    try{
+        const { id } = req.params;
+        const taskData = await TaskModel.findById(id);
+        const impTask = taskData.important;
+        await TaskModel.findByIdAndUpdate(id, { 
+            important: !impTask
+        });
+        res.status(200).json({ msg: 'task updated' });
+    }catch(err){
+        console.log(err);
+        res.status(400).json({msg: "Internal Server Error"});
+    }
+});
+
+//update complete tasl
+router.put('/updateCompleteTask/:id', authenticateToken, async(req, res) => {
+    try{
+        const { id } = req.params;
+        const taskData = await TaskModel.findById(id);
+        const compTask = taskData.complete;
+        await TaskModel.findByIdAndUpdate(id, { 
+            complete: !compTask
+        });
+        res.status(200).json({ msg: 'task updated' });
+    }catch(err){
+        console.log(err);
+        res.status(400).json({msg: "Internal Server Error"});
+    }
+});
+
+//get all important tasks
+router.get('/impTasks', authenticateToken, async(req, res) => {
+    try{
+        const { id } = req.headers;
+        const data = await UserModel.findById(id).populate({
+            path: "tasks",
+            match: { important: true },
+            options: { sort: { createdAt: -1 } }
+        });
+
+        const impTaskData = data.tasks;
+        
+        res.status(200).json({ data: impTaskData });
+    }catch(err){
+        console.log(err);
+        res.status(400).json({msg: "Internal Server Error"});
+    }
+});
+
+//get all important tasks
+router.get('/completeTasks', authenticateToken, async(req, res) => {
+    try{
+        const { id } = req.headers;
+        const data = await UserModel.findById(id).populate({
+            path: "tasks",
+            match: { complete: true },
+            options: { sort: { createdAt: -1 } }
+        });
+
+        const compTaskData = data.tasks;
+        
+        res.status(200).json({ data: compTaskData });
+    }catch(err){
+        console.log(err);
+        res.status(400).json({msg: "Internal Server Error"});
+    }
+});
+
+//get all important tasks
+router.get('/incompleteTasks', authenticateToken, async(req, res) => {
+    try{
+        const { id } = req.headers;
+        const data = await UserModel.findById(id).populate({
+            path: "tasks",
+            match: { complete: false },
+            options: { sort: { createdAt: -1 } }
+        });
+
+        const incompTaskData = data.tasks;
+        
+        res.status(200).json({ data: incompTaskData });
     }catch(err){
         console.log(err);
         res.status(400).json({msg: "Internal Server Error"});
