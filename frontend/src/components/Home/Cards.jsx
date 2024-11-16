f3ab9f36bf4eb9181a62a7628d06eea10649b232
abhisheknaik27 <abhisheknaik2704@gmail.com>
2024-11-17 04:11:39 +0530

    integrated task data into cards

--- frontend/src/components/Home/Cards.jsx
+++ frontend/src/components/Home/Cards.jsx
@@ -4,43 +4,19 @@ import { FaEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 import { IoIosAddCircle } from "react-icons/io";
 
-const Cards = ({ home, setInputDiv }) => {
+const Cards = ({ home, setInputDiv, data }) => {
 
-  const data = [
-    {
-        title: 'Go to gym',
-        desc: "from 7-10",
-        status: "Incomplete"
-    },
-    {
-        title: 'Do project',
-        desc: "from 10-12",
-        status: "Complete"
-    },
-    {
-        title: 'Study',
-        desc: "from 1-4",
-        status: "Complete"
-    },
-    {
-        title: 'Walking',
-        desc: "from 7-10",
-        status: "Incomplete"
-    },
-  ];
-
-  
   return (
     <div className='grid grid-cols-3 gap-4 p-4'>
         {data && data.map((item, i) => (
             <div className='flex flex-col justify-between bg-gray-800 rounded-sm p-4'>
                 <div>
                     <h3 className='text-xl font-semibold'>{item.title}</h3>
-                    <p className='text-gray-300 my-2'>{item.desc}</p>
+                    <p className='text-gray-300 my-2'>{item.description}</p>
                 </div>
                 <div className='mt-4 w-full flex justify-between items-center'>
-                    <button className={`${item.status == "Incomplete" ? 'bg-red-400' : 'bg-green-600'} rounded p-2`}>
-                        {item.status }
+                    <button className={`${item.complete == false ? 'bg-red-400' : 'bg-green-600'} rounded p-2`}>
+                        {item.complete === true ? "Completed" : "Incomplete"}
                     </button>
                     <div className='text-white p-2 w-3/6 text-2xl font-semibold flex justify-around'>
                         <button><CiHeart /></button>
--- frontend/src/pages/AllTasks.jsx
+++ frontend/src/pages/AllTasks.jsx
@@ -1,11 +1,24 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Cards from '../components/Home/Cards'
 import { IoIosAddCircle } from "react-icons/io";
 import InputData from '../components/Home/InputData';
+import axios from 'axios';
 
 const AllTasks = () => {
   const [inputDiv ,setInputDiv] = useState('hidden');
-  
+  const [taskData, setTaskData] = useState();
+  const headers = { 
+    id: localStorage.getItem("id"), 
+    authorization: `Bearer ${localStorage.getItem("token")}`,
+  };
+  useEffect(() => {
+    const fetch = async() => {
+      const response = await axios.get("http://localhost:3000/api/v2/allTasks", {headers});
+      setTaskData(response.data.data);
+    };
+    fetch()
+  }, [])
+  console.log(taskData); 
   return (
     <>
       <div>
@@ -14,7 +27,7 @@ const AllTasks = () => {
               <IoIosAddCircle className='text-4xl text-gray-400 hover:text-gray-100 transition-all duration-300'/>
             </button>
         </div>
-        <Cards home={"true"} setInputDiv={setInputDiv}/>
+        {taskData && <Cards home={"true"} setInputDiv={setInputDiv} data={taskData.tasks}/>}
       </div>
       <InputData inputDiv={inputDiv} setInputDiv={setInputDiv}/>
     </>
