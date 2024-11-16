9e66fa5e8172c2d2c63dff476f79c86238237f04
abhisheknaik27 <abhisheknaik2704@gmail.com>
2024-11-16 17:03:22 +0530

    signin Api created and tested

--- backend/app.js
+++ backend/app.js
@@ -2,16 +2,19 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
+const userApi = require('./routes/user');
 
-const connectMongoose = require('./connection');
+app.use(express.json());
 
 app.use(cors());
-
+const connectMongoose = require('./connection');
 connectMongoose();
 
-app.use('/', (req, res) => {
-    res.send('hello from backend');
-});
+// app.use('/', (req, res) => {
+//     res.send('hello from backend');
+// });
+
+app.use('/api/v1', userApi);
 
 const PORT = 3000 || process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,2 +1,44 @@
+const router = require('express').Router();
+const UserModel = require('../models/User');
 
+//signin api
+router.post('/signin', async(req, res) => {
+    try{
+        const username = req.body.username;
+        const email = req.body.email;
+        
+        const existingUser = await UserModel.findOne({ username });
+        const existingEmail = await UserModel.findOne({ email });
+        
+        if(existingUser){
+            return res.status(400).json({
+                msg: "UserName Already Exists"
+            });
+        } else if(username.length < 4){
+            return res.status(400).json({
+                msg: "Username should have atleast 4 characters"
+            });
+        }
+        if(existingEmail){
+            return res.status(400).json({
+                msg: "Email Already Exists"
+            });
+        }
+        
+        await UserModel.create({
+            username: req.body.username, 
+            email: req.body.email,
+            password: req.body.password,
+        });
+        return res.status(200).json({ msg: 'Signed in successfully'});
+        
+    }catch(err){
+        console.log(err);
+        res.status(400).json({
+            msg: "Internal Server Error"
+        });
+    }
 
+})
+
+module.exports = router;
